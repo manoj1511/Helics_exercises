1165fcb070100cad3e8fd23a5e837d10ed406247
Cebol Sundarrajan <cebomano@falcon2.ib0.ice.inl.gov>
2019-07-17 14:19:42 -0600

    further excercises

---
+++ excercise_4/worker.py
@@ -0,0 +1,11 @@
+import staff
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("worker_file_name", type = str)
+parser.add_argument("manager_name", type = str)
+args = parser.parse_args()
+print(args.worker_file_name)
+print(args.manager_name)
+
+staff.init_worker(args.worker_file_name, args.manager_name)
