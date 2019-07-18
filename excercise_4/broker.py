9377db9c603e7ab338d58da88282009096fa8ee5
Manoj Kumar Cebol Sundarrajan <csmanojkumar1995@gmail.com>
2019-07-18 09:42:24 -0600

    Mpi federates working now

--- excercise_4/broker.py
+++ excercise_4/broker.py
@@ -5,9 +5,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("num_federates", type = int)
 parser.add_argument("broker_name", type = str)
 args = parser.parse_args()
-print(args.num_federates)
-print(args.broker_name)
-print(type(args.broker_name))
+#print(args.num_federates)
+#print(args.broker_name)
+#print(type(args.broker_name))
 
+print("Starting Broker")
 staff.init_broker(args.num_federates, args.broker_name)
 
